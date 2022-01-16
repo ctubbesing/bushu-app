5c74ff6c90236bc4e6db29f042add2cb7acb7581
Connor Tubbesing <connortubbesing@gmail.com>
2022-01-15 18:29:28 -0600

    Clean up Home margins

--- bushu-app/src/views/Home.vue
+++ bushu-app/src/views/Home.vue
@@ -3,7 +3,7 @@
     <img
       src="../assets/b314_icon_inner.svg"
       alt="bushu314 logo"
-      style="max-width: 300px"
+      style="margin-top: 50px; max-width: 300px"
     />
     <HelloWorld msg="Welcome to BushuApp">
       text
