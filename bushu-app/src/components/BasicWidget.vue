88cb41b5ed0b7300f4edf33adb738bd06c6790d4
Connor Tubbesing <connortubbesing@gmail.com>
2022-01-23 12:59:06 -0600

    Add basic widget component

---
+++ bushu-app/src/components/BasicWidget.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-body">
+    <!-- Header -->
+    <div>
+      <h3>
+        <slot name="header"></slot>
+      </h3>
+    </div>
+    <!-- maybe add options list here -->
+    <!-- Body -->
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'BasicWidget'
+})
+</script>
+
+<style scoped>
+.main-body {
+  display: inline-block;
+  margin: 5px;
+  padding: 10px;
+  background-color: #fff9;
+  border-radius: 8px;
+}
+</style>
--- bushu-app/src/views/Home.vue
+++ bushu-app/src/views/Home.vue
@@ -3,24 +3,45 @@
     <img
       src="@/assets/b314_icon_inner.svg"
       alt="bushu314 logo"
-      style="margin-top: 50px; max-width: 300px"
+      style="margin-top: 50px; max-width: 250px"
     />
-    <HelloWorld msg="Welcome to BushuApp">
-      text
-      <b-badge>badge</b-badge>
-      <b-spinner type="grow" />
-    </HelloWorld>
+    <div class="all-widgets">
+      <basic-widget
+        v-for="num in 5"
+        :key="num"
+      >
+        <template v-slot:header>
+          Test Widget {{ num }}
+        </template>
+        <div>
+          test content a
+        </div>
+        <div>
+          test content b
+        </div>
+        <b-button>Test Button</b-button>
+      </basic-widget>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import BasicWidget from "@/components/BasicWidget.vue"
 
 export default Vue.extend({
   name: "Home",
   components: {
-    HelloWorld,
+    BasicWidget: BasicWidget,
   },
 });
 </script>
+
+<style scoped>
+.all-widgets {
+  margin: 20px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #fff5;
+}
+</style>
