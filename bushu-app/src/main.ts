e5b8a088d7fc8d60f9ef8a216685b9d4767a16e9
Connor Tubbesing <connortubbesing@gmail.com>
2023-05-15 18:14:22 -0500

    Install vue-cookies

--- bushu-app/src/main.ts
+++ bushu-app/src/main.ts
@@ -3,11 +3,13 @@ import App from "./App.vue"
 import router from "./router"
 import store from "./store"
 import BootstrapVue from "bootstrap-vue"
+import VueCookies from 'vue-cookies'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue)
+Vue.use(VueCookies)
 
 new Vue({
   router,
