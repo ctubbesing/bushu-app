33870976309fc1285a19609610f92ab94dfd8943
Connor Tubbesing <connortubbesing@gmail.com>
2023-11-03 21:43:07 -0500

    Allow manual progress edit

--- bushu-app/src/components/Watchlist/WatchlistItem.vue
+++ bushu-app/src/components/Watchlist/WatchlistItem.vue
@@ -20,29 +20,52 @@
             </template>
           </div>
           <div
-            v-if="seasonViewData"
+            v-if="editedSeasonView"
             id="viewed-dates"
           >
-            <div v-if="seasonViewData.beganDate">
-              Started {{ seasonViewData.beganDate | formatDate }}
+            <div v-if="editedSeasonView.beganDate">
+              Started {{ editedSeasonView.beganDate | formatDate }}
             </div>
-            <div v-if="seasonViewData.completedDate">
-              Completed {{ seasonViewData.completedDate | formatDate }}
+            <div v-if="editedSeasonView.completedDate">
+              Completed {{ editedSeasonView.completedDate | formatDate }}
             </div>
           </div>
         </div>
       </div>
       <div
-        v-if="seasonViewData"
+        v-if="editedSeasonView"
         id="progress-section"
-        :style="getProgressBar(seasonViewData)"
+        :style="getProgressBar(editedSeasonView)"
       >
         <div>
           <span @click="toggleEpisodeCountUnits">
             Progress:
           </span>
           <span>
-            <span>{{ displayedWatchedEpisodeCount ? displayedWatchedEpisodeCount : '-' }}</span>
+            <span
+              v-if="!doManualProgressEdit"
+              @click="enableManualProgressEdit"
+              style="cursor: pointer"
+            >
+              {{ displayedEpisodeProgress ? displayedEpisodeProgress : '-' }}
+            </span>
+            <span
+              v-else
+              style="display: inline-block"
+            >
+              <b-form-input
+                type="number"
+                v-model.number="editedProgress"
+                :max="maxValidProgress"
+                :min="minValidProgress"
+                :state="isEditedProgressValid ? null : false"
+                size="sm"
+                style="width: 75px; font-size: 0.8em"
+                autofocus
+                @blur="saveManualProgressEdit"
+                @keyup.enter="saveManualProgressEdit"
+              />
+            </span>
             <span> / {{ displayedTotalEpisodeCount ? displayedTotalEpisodeCount : '?' }}</span>
           </span>
           <span
@@ -106,9 +129,11 @@ export default Vue.extend({
   },
   data() {
     return {
-      seasonViewData: null as SeasonView | null,
+      editedSeasonView: null as SeasonView | null,
       doEpisodeCountOverall: false as boolean,
       doEpisodeCountUnitsLabel: false as boolean,
+      doManualProgressEdit: false as boolean,
+      editedProgress: 0 as number,
     }
   },
   computed: {
@@ -116,21 +141,21 @@ export default Vue.extend({
       if (this.showInfo) {
         return this.showInfo
       }
-      const showSeasonData = this.seasonViewData ? this.seasonViewData.seasonInfo : this.showSeason
+      const showSeasonData = this.editedSeasonView ? this.editedSeasonView.seasonInfo : this.showSeason
       return this.$store.getters.getShowInfoById(showSeasonData.showId)
     },
     loadedShowSeason(): ShowSeason | null {
       if (this.showSeason) {
         return this.showSeason
       }
-      if (this.seasonViewData) {
-        return this.seasonViewData.seasonInfo
+      if (this.editedSeasonView) {
+        return this.editedSeasonView.seasonInfo
       }
       return null
     },
     imageLink(): string {
-      if (this.seasonViewData || this.showSeason) {
-        const showSeasonData = this.seasonViewData ? this.seasonViewData.seasonInfo : this.showSeason
+      if (this.editedSeasonView || this.showSeason) {
+        const showSeasonData = this.editedSeasonView ? this.editedSeasonView.seasonInfo : this.showSeason
         if (showSeasonData.imgLink) {
           return showSeasonData.imgLink
         }
@@ -140,28 +165,34 @@ export default Vue.extend({
       }
       return ''
     },
-    overallWatchedEpisodeCount(): number {
-      if (this.seasonViewData) {
-        const currentSznNumber = this.seasonViewData.seasonInfo.seasonNumber
-        return this.seasonViewData.watchedEpisodes + this.loadedShowInfo.seasons.reduce((total: number, szn: ShowSeason) => {
+    previousSeasonsEpisodeCount(): number {
+      if (this.loadedShowSeason) {
+        const currentSznNumber = this.loadedShowSeason.seasonNumber
+        return this.loadedShowInfo.seasons.reduce((total: number, szn: ShowSeason) => {
           return total + ((currentSznNumber > szn.seasonNumber && szn.totalEpisodeCount) ? szn.totalEpisodeCount : 0)
         }, 0)
       }
       return 0
     },
+    overallEpisodeProgress(): number {
+      if (this.editedSeasonView) {
+        return this.editedSeasonView.watchedEpisodes + this.previousSeasonsEpisodeCount
+      }
+      return 0
+    },
     overallTotalEpisodeCount(): number {
-      if (this.seasonViewData) {
+      if (this.editedSeasonView) {
         return this.loadedShowInfo.seasons.reduce((total: number, szn: ShowSeason) => {
           return total + (szn.totalEpisodeCount ? szn.totalEpisodeCount : 0)
         }, 0)
       }
       return 0
     },
-    displayedWatchedEpisodeCount(): number {
+    displayedEpisodeProgress(): number {
       if (this.doEpisodeCountOverall) {
-        return this.overallWatchedEpisodeCount
+        return this.overallEpisodeProgress
       } else {
-        return this.seasonViewData ? this.seasonViewData.watchedEpisodes : 0
+        return this.editedSeasonView ? this.editedSeasonView.watchedEpisodes : 0
       }
     },
     displayedTotalEpisodeCount(): number {
@@ -171,16 +202,30 @@ export default Vue.extend({
         return (this.loadedShowSeason && this.loadedShowSeason.totalEpisodeCount) ? this.loadedShowSeason.totalEpisodeCount : 0
       }
     },
+    minValidProgress(): number {
+      return this.doEpisodeCountOverall ? this.previousSeasonsEpisodeCount : 0
+    },
+    maxValidProgress(): number {
+      const currentSznTotalEpisodes: number | null | undefined = this.loadedShowSeason?.totalEpisodeCount
+      if (currentSznTotalEpisodes == null) {
+        return Infinity
+      }
+      return currentSznTotalEpisodes + this.minValidProgress
+    },
+    isEditedProgressValid(): boolean {
+      return this.editedProgress >= this.minValidProgress &&
+             this.editedProgress <= this.maxValidProgress
+    },
   },
   created() {
     if (this.seasonView) {
-      this.seasonViewData = this.seasonView
+      this.editedSeasonView = this.seasonView
     }
     this.doEpisodeCountOverall = this.loadedShowInfo.doEpisodeCountOverall || false
   },
   watch: {
     seasonView() {
-      this.seasonViewData = this.seasonView
+      this.editedSeasonView = this.seasonView
     },
   },
   methods: {
@@ -191,21 +236,35 @@ export default Vue.extend({
     },
     getProgressBar(view: SeasonView): string {
       let progressPct = 0
-      progressPct = this.displayedTotalEpisodeCount ? (100 * this.displayedWatchedEpisodeCount / this.displayedTotalEpisodeCount) : 50
+      progressPct = this.displayedTotalEpisodeCount ? (100 * this.displayedEpisodeProgress / this.displayedTotalEpisodeCount) : 50
       return `background-image: linear-gradient(to right, hsl(222, 71%, 60%) ${progressPct}%, hsl(222, 71%, 75%) ${progressPct}%);`
     },
     incrementProgress() {
-      if (this.seasonViewData) {
-        const totalEps = this.seasonViewData.seasonInfo.totalEpisodeCount
-        if (totalEps == null || this.seasonViewData.watchedEpisodes < totalEps) {
-          this.seasonViewData.watchedEpisodes++
-          watchlistService.SaveSeasonViewDebounced(this.seasonViewData)
+      if (this.editedSeasonView) {
+        const totalEps = this.editedSeasonView.seasonInfo.totalEpisodeCount
+        if (totalEps == null || this.editedSeasonView.watchedEpisodes < totalEps) {
+          this.editedSeasonView.watchedEpisodes++
+          watchlistService.SaveSeasonViewDebounced(this.editedSeasonView)
         }
       }
     },
     toggleEpisodeCountUnits() {
       this.doEpisodeCountUnitsLabel = true
       this.doEpisodeCountOverall = !this.doEpisodeCountOverall
+    },
+    enableManualProgressEdit() {
+      this.doManualProgressEdit = true
+      this.editedProgress = this.displayedEpisodeProgress
+    },
+    saveManualProgressEdit() {
+      if (this.editedSeasonView) {
+        if (this.isEditedProgressValid && this.editedProgress !== this.displayedEpisodeProgress) {
+          const seasonProgress = this.doEpisodeCountOverall ? (this.editedProgress - this.previousSeasonsEpisodeCount) : this.editedProgress
+          this.editedSeasonView.watchedEpisodes = seasonProgress
+          watchlistService.SaveSeasonViewDebounced(this.editedSeasonView)
+        }
+        this.doManualProgressEdit = false
+      }
     },
   },
 });
