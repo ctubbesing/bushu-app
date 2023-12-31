9df6287d4145f4bc98a21e8142950f382ca2771e
Connor Tubbesing <connortubbesing@gmail.com>
2023-12-30 21:53:35 -0600

    Create IconButton component

---
+++ bushu-app/src/components/utils/IconButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-button
+    :variant="variant ? variant : 'outline-secondary'"
+    pill
+    @click="$emit('click')"
+    style="padding: 12px 14px"
+  >
+    <b-icon
+      :icon="icon"
+      :font-scale="scale ? scale : '1'"
+      style="cursor: pointer; margin-bottom: -2px;"
+    />
+  </b-button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IconButton',
+  props: {
+    icon: String,
+    scale: String,
+    variant: String,
+  },
+})
+</script>
