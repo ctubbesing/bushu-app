18b52425bff83365c87931a8c516631b6299790b
Connor Tubbesing <connortubbesing@gmail.com>
2024-04-04 19:03:46 -0500

    Test new ScrollToRefresh component

--- bushu-app/src/views/Home.vue
+++ bushu-app/src/views/Home.vue
@@ -1,70 +1,63 @@
 <template>
   <div class="home">
-    <b-icon
+    <scroll-to-refresh @refresh="handleScrollToRefresh" />
+    <!-- <b-icon
       icon="hexagon"
       font-scale="2"
       :animation="loading ? 'spin' : ''"
       class="loading-icon"
-    />
+    /> -->
     <img
       src="@/assets/b314_icon_inner.svg"
       alt="bushu314 logo"
       style="margin-top: 50px; max-width: 250px"
     />
     <widget-list
-      :refreshTrigger="refreshTrigger"
+      :refreshTrigger="refreshKey"
     />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import WidgetList from "@/components/WidgetList.vue";
-import dropbox from "@/utils/dropbox";
+import Vue from 'vue'
+import ScrollToRefresh from '@/components/utils/ScrollToRefresh.vue'
+import WidgetList from '@/components/WidgetList.vue'
+import dropbox from '@/utils/dropbox'
 
 export default Vue.extend({
   name: "Home",
   components: {
     widgetList: WidgetList,
+    scrollToRefresh: ScrollToRefresh,
   },
   data() {
     return {
       doRefresh: false as boolean,
-      refreshTrigger: '' as string,
+      refreshKey: '' as string,
       loading: false as boolean,
     };
   },
-  created () {
-    window.addEventListener('scroll', this.handleScroll);
-  },
-  destroyed () {
-    window.removeEventListener('scroll', this.handleScroll);
-  },
-  watch: {
-    async doRefresh() {
-      if (this.doRefresh) {
-        this.loading = true
-        this.refreshTrigger = Date.now().toString()
-        await dropbox.reloadAll()
-        this.loading = false
-      }
-    },
-  },
   methods: {
-    handleScroll() {
-      this.doRefresh = window.scrollY < -50
+    async handleScrollToRefresh(key: string) {
+      this.loading = true
+      this.refreshKey = key
+      await this.refreshPage()
+      this.loading = false
+    },
+    async refreshPage() {
+      await dropbox.reloadAll()
     },
   },
 });
 </script>
 
 <style scoped>
-.loading-icon {
+/* .loading-icon {
   position: absolute;
   left: 0;
   right: 0;
   margin-top: -50px;
   margin-left: auto;
   margin-right: auto;
-}
+} */
 </style>
