f7d57d213b2db656c5ba38290dc63338f53cd7d7
Connor Tubbesing <connortubbesing@gmail.com>
2024-04-11 20:03:59 -0500

    Create custom loading icon

---
+++ bushu-app/src/components/utils/LoadingIcon.vue
@@ -0,0 +1,133 @@
+<template>
+  <svg
+    id="loading-icon"
+    viewBox="0 0 10 13"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <defs>
+      <linearGradient
+        id="myGradient"
+        gradientTransform="rotate(90)"
+      >
+        <stop offset="5%" stop-color="gold" />
+        <stop offset="95%" stop-color="red" />
+      </linearGradient>
+    </defs>
+    <g>
+      <rect
+        x="0"
+        y="0"
+        width="10"
+        height="13"
+        stroke="none"
+        :fill="boxColor"
+        rx="2.5"
+      />
+      <g
+        fill="none"
+        stroke-width="1"
+        stroke-linejoin="round"
+        stroke-linecap="round"
+      >
+        <path
+          :d="iconPath"
+          stroke-dasharray="32.5"
+          :stroke="pathColor"
+        />
+        <path
+          :d="iconPath"
+          stroke-dasharray="16 16"
+          :stroke="loadingColorA"
+        >
+          <animate
+            attributeName="stroke-dashoffset"
+            values="16; 48"
+            :begin="'0s'"
+            :dur="'2s'"
+            repeatCount="indefinite"
+          />
+        </path>
+        <path
+          :d="iconPath"
+          :stroke="loadingColorB"
+          stroke-dasharray="16 16"
+        >
+          <animate
+            attributeName="stroke-dashoffset"
+            values="0; -32"
+            :begin="'0s'"
+            :dur="'2s'"
+            repeatCount="indefinite"
+          />
+        </path>
+        <path
+          v-if="!isLoading"
+          :d="iconPath"
+          stroke="url('#myGradient')"
+        >
+          <animate
+            class="end-animate"
+            attributeName="stroke-dasharray"
+            values="0 32; 32 0"
+            begin="indefinite"
+            :dur="'0.5s'"
+          />
+          <animate
+            class="end-animate"
+            attributeName="stroke-dashoffset"
+            values="0; 16"
+            begin="indefinite"
+            :dur="'0.5s'"
+          />
+        </path>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LoadingIcon',
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      iconPath: `
+        M 2.5 6.5
+        l 0 4
+        l 5 0
+        l 0 -1
+        l -3 -3
+        l -1.757359313 0
+        l 1.757359313 0
+        l 3 -3
+        l 0 -1
+        l -5 0
+        z
+      ` as string,
+      boxColor: '#043' as string,
+      pathColor: '#dfe7' as string,
+      loadingColorA: '#fd8a' as string,
+      loadingColorB: '#8dfa' as string,
+    }
+  },
+  watch: {
+    isLoading(loading: boolean) {
+      if (!loading) {
+        this.$nextTick(() => {
+          let elements = document.getElementsByClassName('end-animate') as unknown as SVGAnimateElement[]
+          for (let i = 0; i < elements.length; i++) {
+            elements[i].beginElement()
+          }
+        })
+      }
+    },
+  },
+})
+</script>
