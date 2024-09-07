692cb2b9e8c54740446061f50d92a7d22d6efa38
Connor Tubbesing <connortubbesing@gmail.com>
2024-09-07 17:56:41 -0500

    Install Vuetify

--- bushu-app/src/main.ts
+++ bushu-app/src/main.ts
@@ -4,13 +4,22 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VueCookies from 'vue-cookies'
 
+import 'vuetify/styles'
+// import 'roboto-fontface/css/roboto/roboto-fontface.css'
+// import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const vuetify = createVuetify()
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
+app.use(vuetify)
 app.use(VueCookies)
 
 app.mount('#app')
