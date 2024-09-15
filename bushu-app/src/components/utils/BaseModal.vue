80b7280c59ad1ae20790fcbd4261b20218284f40
Connor Tubbesing <connortubbesing@gmail.com>
2024-09-14 19:09:26 -0500

    Create BaseModal component

---
+++ bushu-app/src/components/utils/BaseModal.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-dialog
+    v-model="isOpenModel"
+    width="auto"
+  >
+    <v-card
+      max-width="500"
+      :prepend-icon="titleIcon"
+      :title="title"
+      :subtitle="subtitle"
+    >
+      <template #actions>
+        <slot name="actions">
+          <v-btn
+            v-if="doCancel"
+            :text="cancelText"
+            :color="cancelColor"
+            :variant="cancelVariant"
+            @click="$emit('cancel')"
+          />
+          <v-btn
+            v-if="doOk"
+            :text="okText"
+            :color="okColor"
+            :variant="okVariant"
+            @click="$emit('ok')"
+          />
+        </slot>
+      </template>
+      <template #text>
+        <slot></slot>
+      </template>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+import { watch } from 'vue';
+import { VBtn, VCard, VDialog } from 'vuetify/components'
+
+defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+  titleIcon: {
+    type: String,
+    required: false,
+  },
+  subtitle: {
+    type: String,
+    required: false,
+  },
+  doOk: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  doCancel: {
+    type: Boolean,
+    required: false,
+    default: true,
+  },
+  okText: {
+    type: String,
+    required: false,
+    default: 'Ok'
+  },
+  okColor: {
+    type: String,
+    required: false,
+    default: 'green'
+  },
+  okVariant: {
+    type: String as () => 'elevated' | 'tonal' | 'flat' | 'text' | 'outlined' | 'plain',
+    required: false,
+    default: 'flat'
+  },
+  cancelText: {
+    type: String,
+    required: false,
+    default: 'Cancel'
+  },
+  cancelColor: {
+    type: String,
+    required: false,
+    default: 'red'
+  },
+  cancelVariant: {
+    type: String as () => 'elevated' | 'tonal' | 'flat' | 'text' | 'outlined' | 'plain',
+    required: false,
+    default: 'tonal'
+  },
+})
+
+const emit = defineEmits(['ok', 'cancel', 'hidden'])
+
+const isOpenModel = defineModel<boolean>()
+
+watch(isOpenModel, () => {
+  if (!isOpenModel.value) {
+    emit('hidden')
+  }
+})
+</script>
--- bushu-app/src/plugins/BaseUtils.ts
+++ bushu-app/src/plugins/BaseUtils.ts
@@ -2,11 +2,13 @@ import type { App } from 'vue'
 import BaseButton from '@/components/utils/BaseButton.vue'
 import BaseDropdown from '@/components/utils/BaseDropdown.vue'
 import BaseLoader from '@/components/utils/BaseLoader.vue'
+import BaseModal from '@/components/utils/BaseModal.vue'
 
 const loadBaseUtils = (app: App) => {
   app.component('BaseButton', BaseButton)
   app.component('BaseDropdown', BaseDropdown)
   app.component('BaseLoader', BaseLoader)
+  app.component('BaseModal', BaseModal)
 }
 
 export default loadBaseUtils
