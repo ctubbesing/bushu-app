f33e64407e8b2d62a405d233aee2f9649ffc44df
Connor Tubbesing <connortubbesing@gmail.com>
2024-09-15 20:22:22 -0500

    Create BaseTextField component

---
+++ bushu-app/src/components/utils/BaseTextField.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-text-field
+    v-model="textModel"
+    :label="label"
+  >
+    <template #append>
+      <v-btn
+        v-if="doOkButton"
+        :text="okText"
+        :color="okColor"
+        :variant="okVariant"
+        @click="$emit('ok')"
+      />
+      <slot name="append"></slot>
+    </template>
+  </v-text-field>
+</template>
+
+<script lang="ts" setup>
+import { VBtn } from 'vuetify/components/VBtn'
+import { VTextField } from 'vuetify/components/VTextField'
+
+const textModel = defineModel<string>()
+
+defineEmits(['ok'])
+
+defineProps({
+  label: {
+    type: String,
+    required: false,
+  },
+  doOkButton: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  okText: {
+    type: String,
+    required: false,
+    default: 'Ok'
+  },
+  okColor: {
+    type: String,
+    required: false,
+    default: '#333'
+  },
+  okVariant: {
+    type: String as () => 'elevated' | 'tonal' | 'flat' | 'text' | 'outlined' | 'plain',
+    required: false,
+    default: 'elevated'
+  },
+})
+</script>
--- bushu-app/src/plugins/BaseUtils.ts
+++ bushu-app/src/plugins/BaseUtils.ts
@@ -3,12 +3,14 @@ import BaseButton from '@/components/utils/BaseButton.vue'
 import BaseDropdown from '@/components/utils/BaseDropdown.vue'
 import BaseLoader from '@/components/utils/BaseLoader.vue'
 import BaseModal from '@/components/utils/BaseModal.vue'
+import BaseTextField from '@/components/utils/BaseTextField.vue'
 
 const loadBaseUtils = (app: App) => {
   app.component('BaseButton', BaseButton)
   app.component('BaseDropdown', BaseDropdown)
   app.component('BaseLoader', BaseLoader)
   app.component('BaseModal', BaseModal)
+  app.component('BaseTextField', BaseTextField)
 }
 
 export default loadBaseUtils
