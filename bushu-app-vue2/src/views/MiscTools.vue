6a36b2db0761e6acdaa2cf8ca39a1fb712a873bb
Connor Tubbesing <connortubbesing@gmail.com>
2025-03-09 20:01:50 -0500

    Add Droplet Ring to Misc Tools

--- bushu-app-vue2/src/views/MiscTools.vue
+++ bushu-app-vue2/src/views/MiscTools.vue
@@ -15,15 +15,26 @@
         <b-tab title="Ray Tracer">
           <canvas-test />
         </b-tab>
-        <b-tab title="Curve Creator">
+        <!-- <b-tab title="Curve Creator">
           <curve-creator />
-        </b-tab>
+        </b-tab> -->
         <b-tab title="Sandbox">
           <sandbox-game />
         </b-tab>
-        <!-- <b-tab title="TouchDragCanvasTest">
-          <TouchDragCanvasTest />
-        </b-tab> -->
+        <b-tab title="Droplet Ring">
+          <b-form-spinbutton
+            v-model="dropletCount"
+            min="2"
+            max="10"
+            wrap
+            inline
+            style="margin:10px;"
+          />
+          <droplet-ring
+            :n="dropletCount"
+            style="max-width: 500px;"
+          />
+        </b-tab>
       </b-tabs>
     </b-card>
   </div>
@@ -33,17 +44,23 @@
 import Vue from 'vue'
 import ImageAnalyzer from '@/components/miscTools/ImageAnalyzer/ImageAnalyzer.vue'
 import CanvasTest from '@/components/CanvasTest.vue'
-import CurveCreator from '@/components/miscTools/CurveCreator.vue'
+// import CurveCreator from '@/components/miscTools/CurveCreator.vue'
 import SandboxGame from '@/components/miscTools/SandboxGame.vue'
-// import TouchDragCanvasTest from '@/components/miscTools/TouchDragCanvasTest.vue'
+import DropletRing from '@/components/miscTools/DropletRing.vue'
 
 export default Vue.extend({
   name: "MiscTools",
   components: {
     canvasTest: CanvasTest,
     imageAnalyzer: ImageAnalyzer,
-    curveCreator: CurveCreator,
+    // curveCreator: CurveCreator,
     sandboxGame: SandboxGame,
+    dropletRing: DropletRing,
+  },
+  data() {
+    return {
+      dropletCount: 3,
+    }
   },
 });
 </script>
