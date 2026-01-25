fae1ae4e155fda1f38107621a91e9ecb367f0018
Connor Tubbesing <connortubbesing@gmail.com>
2026-01-25 17:13:03 -0600

    Begin rewriting WatchlistView in composition API

--- bushu-app/src/views/WatchlistView.vue
+++ bushu-app/src/views/WatchlistView.vue
@@ -13,8 +13,8 @@
           icon="mdi-television-guide"
           color="app-gray"
           variant="outlined"
-          @click="openCatalog()"
         />
+          <!-- @click="openCatalog()" -->
       </div>
       <div id="active-sections-container">
         <div id="currently-watching-section">
@@ -22,6 +22,14 @@
         </div>
         <div id="main-live-container">
           <div id="main-list">
+            <WatchlistSection
+              v-model:season-views="watchlist.main"
+              :list-type="'Main'"
+              @add-item="console.log('@add-item -> selectCatalogEntry(Live)')"
+              @mark-item-completed="console.log('@mark-item-completed -> showMarkCompletedOptions')"
+              @demote-item="console.log('@demote-item -> demoteItem')"
+              @remove-item="console.log('@remove-item -> promptConfirmRemoveItem')"
+            />
             <!-- <watchlist-section /////////////// TODO: fix
               v-model="watchlist.main"
               :list-type="'main'"
@@ -32,6 +40,16 @@
             /> -->
           </div>
           <div id="live-list">
+            <WatchlistSection
+              v-model:season-views="watchlist.live"
+              :list-type="'Live'"
+              @add-item="console.log('@add-item -> selectCatalogEntry(Live)')"
+              @mark-item-completed="console.log('@mark-item-completed -> showMarkCompletedOptions')"
+              @promote-item="console.log('@promote-item -> promoteItem')"
+              @demote-item="console.log('@demote-item -> demoteItem')"
+              @remove-item="console.log('@remove-item -> promptConfirmRemoveItem')"
+              @irregular-seasons-updated="console.log('@irregular-seasons-updated -> loadWatchlist()')"
+            />
             <!-- <watchlist-section /////////////// TODO: fix
               v-model="watchlist.live"
               :list-type="'live'"
@@ -47,6 +65,14 @@
       </div>
       <div id="inactive-sections-container">
         <div id="queue-list">
+          <WatchlistSection
+            v-model:season-views="watchlist.queue"
+            :list-type="'Queue'"
+            @add-item="console.log('QUEUE @add-item -> selectCatalogEntry(Queue)' + $event)"
+            @promote-item="console.log('QUEUE @promote-item -> promoteItem' + $event + $event)"
+            @remove-item="console.log('QUEUE @remove-item -> promptConfirmRemoveItem' + $event)"
+            @reorder="console.log('QUEUE @reorder -> onReorderList' + $event)"
+          />
           <!-- <watchlist-section /////////////// TODO: fix
             v-model="watchlist.queue"
             :list-type="'queue'"
@@ -57,6 +83,14 @@
           /> -->
         </div>
         <div id="upcoming-list">
+          <WatchlistSection
+            v-model:seasons="watchlist.upcoming"
+            :list-type="'Upcoming'"
+            @add-item="console.log('@add-item -> selectCatalogEntry(Upcoming)')"
+            @promote-item="console.log('@promote-item -> promoteItem')"
+            @remove-item="console.log('@remove-item -> promptConfirmRemoveItem')"
+            @irregular-seasons-updated="console.log('@irregular-seasons-updated -> loadWatchlist()')"
+          />
           <!-- <watchlist-section /////////////// TODO: fix
             v-model="watchlist.upcoming"
             :list-type="'upcoming'"
@@ -67,6 +101,14 @@
           /> -->
         </div>
         <div id="backlog-list">
+          <WatchlistSection
+            v-model:shows="watchlist.backlog"
+            :list-type="'Backlog'"
+            @add-item="console.log('@add-item -> selectCatalogEntry(Backlog)')"
+            @promote-item="console.log('@promote-item -> promoteItem')"
+            @remove-item="console.log('@remove-item -> promptConfirmRemoveItem')"
+            @reorder="console.log('@reorder -> onReorderList')"
+          />
           <!-- <watchlist-section /////////////// TODO: fix
             v-model="watchlist.backlog"
             :list-type="'backlog'"
@@ -154,332 +196,42 @@
   </div>
 </template>
 
-<script lang="ts">
-// import WatchlistSection from '@/components/Watchlist/WatchlistSection.vue'
+<script lang="ts" setup>
+import WatchlistSection from '@/components/Watchlist/WatchlistSection.vue'
 import { useDropbox } from '@/stores/dropbox'
-import { useHome } from '@/stores/home'
 import { useWatchlist } from '@/stores/watchlist'
-import type {
-  SeasonView,
-  ShowInfo,
-  ShowSeason,
-  WatchlistData,
-} from '@/types/watchlistTypes'
-import WatchlistService from '@/utils/services/WatchlistService'
-import tools from '@/utils/tools'
-import toTitleCase from '@/utils/toTitleCase'
-import { mapStores } from 'pinia'
+import { WatchlistData } from '@/types/watchlistTypes'
+import watchlistService from '@/utils/services/watchlistService'
+import { ref, watch } from 'vue'
 
-export default {
-  components: {
-    // WatchlistSection,
-    // CatalogModal,
-    // WatchlistSection,
-    // WatchlistItem,
-    // IconButton,
-    // ScrollToRefresh,
-  },
-  data() {
-    return {
-      watchlist: null as WatchlistData | null,
-      isLoading: false as boolean,
-      targetListName: null as string | null,
-      targetSeasonView: null as SeasonView | null,
-      targetShowSeason: null as ShowSeason | null,
-      targetShowInfo: null as ShowInfo | null,
-      targetNextQueueItem: null as SeasonView | null,
-      doNextSeasonToQueue: true as boolean,
-      doPopQueueToMain: true as boolean,
-    };
-  },
-  computed: {
-    ...mapStores(useHome, useDropbox, useWatchlist),
-    isReady(): boolean {
-      return this.dropboxStore.isReady
-    },
-  },
-  watch: {
-    async isReady(isReady: boolean) {
-      if (isReady) {
-        await this.loadData()
-      }
-    },
-  },
-  async created() {
-    if (this.isReady) {
-      await this.loadData()
-    }
-  },
-  methods: {
-    toTitleCase(s: string): string {
-      return toTitleCase(s)
-    },
-    resetTargets() {
-      this.targetListName = null
-      this.targetSeasonView = null
-      this.targetShowSeason = null
-      this.targetShowInfo = null
-      this.targetNextQueueItem = null
-    },
-    async loadWatchlist(doForceReload = false) {
-      this.watchlist = await WatchlistService.GetWatchlistData(doForceReload)
-    },
-    async saveWatchlist() {
-      if (this.watchlist) {
-        await WatchlistService.SaveWatchlistData(this.watchlist)
-      }
-    },
-    async loadData(doForceReload = false) {
-      this.isLoading = true
+const watchlistStore = useWatchlist()
+const dropboxStore = useDropbox()
 
-      await this.watchlistStore.loadCatalogFromDropbox()
-      await this.loadWatchlist(doForceReload)
+const isLoading = ref(false)
+const watchlist = ref<WatchlistData>()
 
-      this.isLoading = false
-    },
-    async handleRefresh() {
-      this.isLoading = true
+// const loadData = async (doForceReload = false) => {
+//   isLoading.value = true
 
-      await WatchlistService.ForceSaveSeasonViews()
-      await this.loadData(true)
+//   await watchlistStore.loadCatalogFromDropbox()
+//   await this.loadWatchlist(doForceReload)
 
-      this.isLoading = false
-    },
-    selectCatalogEntry(listName: string) {
-      this.targetListName = listName
-      this.openCatalog()
-    },
-    async addCatalogItemToList(item: ShowInfo | ShowSeason, listName: string) {
-      if (this.watchlist) {
-        if (listName === 'main' || listName === 'live' || listName === 'queue') {
-          let newSeasonView: SeasonView = {
-            id: tools.getGUID(),
-            seasonInfo: item as ShowSeason,
-            watchedEpisodes: 0,
-          }
-          this.watchlist[listName].push(newSeasonView)
-        } else if (listName === 'upcoming') {
-          this.watchlist.upcoming.push(item as ShowSeason)
-          this.watchlist.upcoming.sort((a, b) => {
-            return (a.startDate ?? 'z').localeCompare(b.startDate ?? 'z')
-          })
-        } else if (listName === 'backlog') {
-          this.watchlist.backlog.push(item as ShowInfo)
-        }
-      }
-      this.closeCatalog()
-      await this.saveWatchlist()
-    },
-    showMarkCompletedOptions(seasonViewId: string, sourceList: string) {
-      this.targetListName = sourceList
-      if (this.watchlist) {
-        if (sourceList === 'main') {
-          let seasonViewIdx = this.watchlist.main.findIndex((view: SeasonView) => view.id === seasonViewId)
-          if (seasonViewIdx !== -1) {
-            this.doNextSeasonToQueue = false
-            this.doPopQueueToMain = false
-            this.targetSeasonView = this.watchlist.main[seasonViewIdx]
+//   isLoading.value = false
+// }
 
-            // find next season of show, if any
-            this.targetShowSeason = null
-            const seasonInfo = this.targetSeasonView.seasonInfo
-            const seasonShowInfo = this.watchlistStore.getShowInfoById(seasonInfo.showId)
-            if (seasonShowInfo) {
-              let seasonIdx = seasonShowInfo.seasons.findIndex((season: ShowSeason) => season.id === seasonInfo.id)
-              if (seasonIdx < seasonShowInfo.seasons.length - 1) {
-                this.targetShowSeason = seasonShowInfo.seasons[seasonIdx + 1]
-                this.doNextSeasonToQueue = true
-              }
-            }
+const loadData = async (doForceReload: boolean = false) => {
+  if (dropboxStore.isReady) {
+    isLoading.value = true
 
-            // find next item in Queue, if any
-            this.targetNextQueueItem = null
-            if (this.watchlist.queue.length > 0) {
-              this.targetNextQueueItem = this.watchlist.queue[0]
-              this.doPopQueueToMain = true
-            }
+    await watchlistStore.loadCatalog()
+    watchlist.value = await watchlistService.GetWatchlistData(doForceReload)
 
-            // this.$bvModal.show('markCompletedModal') /////////////// TODO: fix
-          }
-        } else if (sourceList === 'live') {
-          let seasonViewIdx = this.watchlist.live.findIndex((view: SeasonView) => view.id === seasonViewId)
-          if (seasonViewIdx !== -1) {
-            this.doNextSeasonToQueue = false
-            this.doPopQueueToMain = false
-            this.targetShowSeason = null
-            this.targetNextQueueItem = null
+    isLoading.value = false
+  }
+}
 
-            this.targetSeasonView = this.watchlist.live[seasonViewIdx]
+watch(() => dropboxStore.isReady, loadData, { immediate: true })
 
-            // this.$bvModal.show('markCompletedModal') /////////////// TODO: fix
-          }
-        }
-      }
-    },
-    async markSeasonCompleted() {
-      if (this.watchlist && this.targetSeasonView) {
-        // set date completed, save, & remove from source list
-        // TODO: eventually should move completed SVs to new save file but for now this works fine
-        this.targetSeasonView.completedDate = tools.getTimestamp()
-        await WatchlistService.SaveSeasonViews([ this.targetSeasonView ])
-
-        if (this.targetListName === 'main' || this.targetListName === 'live') {
-          const completedViewId = this.targetSeasonView.id
-          const completedViewIdx = this.watchlist[this.targetListName].findIndex((view: SeasonView) => view.id === completedViewId)
-          if (completedViewIdx !== -1) {
-            this.watchlist[this.targetListName].splice(completedViewIdx, 1)
-          }
-        }
-
-        if (this.doNextSeasonToQueue) {
-          if (this.targetShowSeason) {
-            const newSeasonView: SeasonView = {
-              id: tools.getGUID(),
-              seasonInfo: this.targetShowSeason,
-              watchedEpisodes: 0,
-            }
-            this.watchlist.queue.push(newSeasonView)
-          }
-        }
-
-        if (this.doPopQueueToMain) {
-          if (this.targetNextQueueItem) {
-            this.watchlist.main.push(this.targetNextQueueItem)
-            this.watchlist.queue.splice(0, 1)
-          }
-        }
-
-        await this.saveWatchlist()
-      }
-      this.resetTargets()
-    },
-    promptConfirmRemoveItem(itemId: string, sourceList: string) {
-      this.targetListName = sourceList
-      if (this.watchlist) {
-        if (sourceList === 'main' || sourceList === 'live' || sourceList === 'queue') {
-          let seasonViewIdx = this.watchlist[sourceList].findIndex((view: SeasonView) => view.id === itemId)
-          this.targetSeasonView = this.watchlist[sourceList][seasonViewIdx]
-        } else if (sourceList === 'upcoming') {
-          let showSeasonIdx = this.watchlist.upcoming.findIndex((szn: ShowSeason) => szn.id === itemId)
-          this.targetShowSeason = this.watchlist.upcoming[showSeasonIdx]
-        } else if (sourceList === 'backlog') {
-          let showInfoIdx = this.watchlist.backlog.findIndex((show: ShowInfo) => show.id === itemId)
-          this.targetShowInfo = this.watchlist.backlog[showInfoIdx]
-        }
-        // this.$bvModal.show('confirmRemoveModal') /////////////// TODO: fix
-      }
-    },
-    async promoteItem(itemId: string, sourceList: string, destinationList: string | undefined = undefined) {
-      if (this.watchlist) {
-        if (sourceList === 'live' || sourceList === 'queue') {
-          // promote Live or Queued SeasonView to Main
-          const seasonViewIdx = this.watchlist[sourceList].findIndex((view: SeasonView) => view.id === itemId)
-          if (seasonViewIdx !== -1) {
-            const promotedSeasonView = this.watchlist[sourceList][seasonViewIdx]
-            this.watchlist.main.push(promotedSeasonView)
-            this.watchlist[sourceList].splice(seasonViewIdx, 1)
-          }
-        } else if (sourceList === 'upcoming') {
-          // promote Upcoming ShowSeason to Live or Main
-          const sznIdx = this.watchlist.upcoming.findIndex((szn: ShowSeason) => szn.id === itemId)
-          if (sznIdx !== -1) {
-            const promotedSzn = this.watchlist.upcoming[sznIdx]
-
-            let newSeasonView: SeasonView = {
-              id: tools.getGUID(),
-              seasonInfo: promotedSzn,
-              watchedEpisodes: 0,
-            }
-            if (!destinationList || destinationList === 'live') {
-              this.watchlist.live.push(newSeasonView)
-            } else if (destinationList === 'main') {
-              this.watchlist.main.push(newSeasonView)
-            }
-
-            this.watchlist.upcoming.splice(sznIdx, 1)
-          }
-        } else if (sourceList === 'backlog') {
-          // promote Backlog ShowInfo to Queue
-          const showInfoIdx = this.watchlist.backlog.findIndex((show: ShowInfo) => show.id === itemId)
-          if (showInfoIdx !== -1) {
-            const promotedShow = this.watchlist.backlog[showInfoIdx]
-            if (promotedShow.seasons.length > 0) {
-              const showFirstSeason = promotedShow.seasons[0]
-
-              let newSeasonView: SeasonView = {
-                id: tools.getGUID(),
-                seasonInfo: showFirstSeason,
-                watchedEpisodes: 0,
-              }
-              this.watchlist.queue.push(newSeasonView)
-            }
-            this.watchlist.backlog.splice(showInfoIdx, 1)
-          }
-        }
-
-        await this.saveWatchlist()
-      }
-    },
-    async demoteItem(itemId: string, sourceList: string) {
-      if (this.watchlist) {
-        if (sourceList === 'main' || sourceList === 'live') {
-          // demote SeasonView from Main or Live to top of Queue
-          const seasonViewIdx = this.watchlist[sourceList].findIndex((view: SeasonView) => view.id === itemId)
-          if (seasonViewIdx !== -1) {
-            const demotedSeasonView = this.watchlist[sourceList][seasonViewIdx]
-            this.watchlist.queue.splice(0, 0, demotedSeasonView)
-            this.watchlist[sourceList].splice(seasonViewIdx, 1)
-          }
-        }
-
-        await this.saveWatchlist()
-      }
-    },
-    async removeItem() {
-      if (this.watchlist) {
-        if ((this.targetListName === 'main' || this.targetListName === 'live' || this.targetListName === 'queue') && this.targetSeasonView) {
-          this.targetSeasonView.droppedDate = tools.getTimestamp()
-          await WatchlistService.SaveSeasonViews([ this.targetSeasonView ])
-
-          const droppedViewId = this.targetSeasonView.id
-          const droppedViewIdx = this.watchlist[this.targetListName].findIndex((view: SeasonView) => view.id === droppedViewId)
-          if (droppedViewIdx !== -1) {
-            this.watchlist[this.targetListName].splice(droppedViewIdx, 1)
-          }
-        } else if (this.targetListName === 'upcoming' && this.targetShowSeason) {
-          const droppedSznId = this.targetShowSeason.id
-          const droppedSznIdx = this.watchlist.upcoming.findIndex((szn: ShowSeason) => szn.id === droppedSznId)
-          if (droppedSznIdx !== -1) {
-            this.watchlist.upcoming.splice(droppedSznIdx, 1)
-          }
-        } else if (this.targetListName === 'backlog' && this.targetShowInfo) {
-          const droppedShowId = this.targetShowInfo.id
-          const droppedShowIdx = this.watchlist.backlog.findIndex((show: ShowInfo) => show.id === droppedShowId)
-          if (droppedShowIdx !== -1) {
-            this.watchlist.backlog.splice(droppedShowIdx, 1)
-          }
-        }
-
-        await this.saveWatchlist()
-      }
-      this.resetTargets()
-    },
-    async onReorderList() {
-      await this.saveWatchlist()
-    },
-    openCatalog() {
-      // this.$bvModal.show('catalogModal') /////////////// TODO: fix
-    },
-    closeCatalog() {
-      // this.$bvModal.hide('catalogModal') /////////////// TODO: fix
-    },
-    // onModalHide(event: BvModalEvent) { /////////////// TODO: fix
-    //   if (event.trigger !== 'ok') {
-    //     this.resetTargets()
-    //   }
-    // },
-  },
-};
 </script>
 
 <style scoped>
