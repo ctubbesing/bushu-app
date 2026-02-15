aaee3ccd71c290b1c89283473b88835f4ecc5f34
Connor Tubbesing <connortubbesing@gmail.com>
2026-02-14 18:35:38 -0600

    Continue rewriting Catalog

--- bushu-app/src/components/Watchlist/CatalogModal.vue
+++ bushu-app/src/components/Watchlist/CatalogModal.vue
@@ -21,10 +21,36 @@
         <v-expansion-panel
           v-for="show in catalog"
           :key="show.id"
-          :title="show.title"
         >
-          <template>
-            asdfasdfasdf
+          <template #title>
+            <div
+              class="d-flex w-100"
+              style="height: 90px;"
+            >
+              <ThumbnailImage
+                :link="watchlistStore.getShowImageLink(show)"
+                :colorSeed="show.id"
+                :height="90"
+                class="mr-2"
+              />
+              <div class="d-flex flex-column justify-space-between h-100">
+                <div>
+                  <h4>{{ show.title }}</h4>
+                  <div
+                    v-if="show.altTitle"
+                    class="text-caption mt-n2"
+                  >
+                    {{ show.altTitle }}
+                  </div>
+                </div>
+                <div v-if="show.seasonCount">
+                  {{  tools.pluralFormat(show.seasonCount, 'season') }}
+                </div>
+                <div v-else>
+                  {{ getTotalEpisodeCount(show) }} total episodes
+                </div>
+              </div>
+            </div>
           </template>
           <template #text>
             <v-list>
@@ -46,8 +72,10 @@
 <script lang="ts" setup>
 import BaseModal from '@/components/utils/BaseModal.vue'
 import { useWatchlist } from '@/stores/watchlist'
-import type { ShowInfo } from '@/types/watchlistTypes'
+import type { ShowInfo, ShowSeason } from '@/types/watchlistTypes'
 import { computed, onMounted, ref } from 'vue'
+import ThumbnailImage from '../ThumbnailImage.vue'
+import tools from '@/utils/tools'
 
 const showModal = defineModel<boolean>()
 
@@ -57,7 +85,16 @@ const watchlistStore = useWatchlist()
 const catalog = computed((): ShowInfo[] => watchlistStore.catalog)
 
 onMounted(() => watchlistStore.loadCatalog())
+
+const getTotalEpisodeCount = (show: ShowInfo): number => {
+  return show.seasons.reduce((sum: number, s: ShowSeason) => {
+    return sum + (s.totalEpisodeCount ? s.totalEpisodeCount : 0)
+  }, 0)
+}
 </script>
 
 <style scoped>
+:deep(.v-expansion-panel-title) {
+  padding: 8px;
+}
 </style>
